223f5f8abf9cf57e026e578f8273a351dc06e0c6
Vladislav_Ozhogov <vladislav.ozhogov@gmail.com>
2026-01-28 10:40:34 +0500

    Transformed adding to deducting

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-import { addFour } from "./libs.js"
+import { deductFour } from "./libs.js"
 import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
 
 function start() {
-    const input = Number(prompt("Hello! Welcome to SumPlus4Twice! Enter your sum: ").trim())
-    console.log(addFour(addFour(input)))
+    const input = Number(
+        prompt("Hello! Welcome to SumMinus4Twice! Enter your sum: ")
+        .trim())
+    console.log(deductFour(deductFour(input)))
 }
 
 start()
