f3c75be4f76eec53a33f98f083e43d70025e4b06
Vladislav_Ozhogov <vladislav.ozhogov@gmail.com>
2026-02-10 17:42:11 +0500

    Revert "Doubled minus process"
    This reverts commit fe4fbc2c75adc40c2a6e6f068bbe5b47491645bb.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const prompt = promptSync()
 function start() {
     const input = Number(prompt("Hello! Welcome to SumPlus3! Enter your sum: ").trim())
 
-    console.log(minusThreeminusThree((input)))
+    console.log(minusThree(input))
 }
 
 start()
