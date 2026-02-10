7a7596a9189f650258fbb731384256f9aaf5e4dd
Vladislav_Ozhogov <vladislav.ozhogov@gmail.com>
2026-02-10 16:53:49 +0500

    Changed from adding to deducting

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import { addThree } from "./libs.js"
+import { addThree, minusThree } from "./libs.js"
 import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
 
 function start() {
     const input = Number(prompt("Hello! Welcome to SumPlus3! Enter your sum: ").trim())
-    console.log(addThree(input))
+    console.log(minusThree(input))
 }
 
 start()
